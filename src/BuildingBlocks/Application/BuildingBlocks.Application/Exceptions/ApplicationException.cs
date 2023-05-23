0ed6e5888520e49f3407a2d7da998982a903daf6
samantonovich@gmail.com <samantonovich@gmail.com>
2023-05-23 22:59:59 +0300

    Add domain and application building blocks

---
+++ src/BuildingBlocks/Application/BuildingBlocks.Application/Exceptions/ApplicationException.cs
@@ -0,0 +1,7 @@
+﻿namespace BuildingBlocks.Application.Exceptions;
+
+public class ApplicationException : Exception
+{
+    public ApplicationException(string message) : base(message)
+    { }
+}
---
+++ src/BuildingBlocks/Domain/BuildingBlocks.Domain/Entity.cs
@@ -0,0 +1,21 @@
+﻿namespace BuildingBlocks.Domain;
+
+public abstract class Entity<TId> : IEquatable<Entity<TId>>
+    where TId : notnull
+{
+    public TId Id { get; protected set; }
+
+    protected Entity(TId id) => Id = id;
+
+    public override bool Equals(object? obj) =>
+        obj is Entity<TId> entity && Id.Equals(entity.Id);
+
+    public override int GetHashCode() => Id.GetHashCode();
+
+    public bool Equals(Entity<TId>? other) =>
+        other is not null && Id.Equals(other.Id);
+
+#pragma warning disable CS8618
+    protected Entity() { }
+#pragma warning restore CS8618
+}
