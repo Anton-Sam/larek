a36e2f4423e40b924664312166932ae940f4ef33
samantonovich@gmail.com <samantonovich@gmail.com>
2023-05-23 23:06:41 +0300

    Add catalog domain

---
+++ src/Services/Catalog/Catalog.Domain/CategoryAggregate/Category.cs
@@ -0,0 +1,18 @@
+﻿using BuildingBlocks.Domain;
+
+namespace Catalog.Domain.CategoryAggregate;
+
+public sealed class Category : AggregateRoot<Guid>
+{
+    public string Name { get; private set; }
+
+    private Category(string name) : base(Guid.NewGuid())
+    {
+        Name = name;
+    }
+
+    public static Category Create(string name)
+    {
+        return new Category(name);
+    }
+}
---
+++ src/Services/Catalog/Catalog.Domain/ItemAggregate/Item.cs
@@ -0,0 +1,76 @@
+﻿using BuildingBlocks.Domain;
+
+namespace Catalog.Domain.ItemAggregate;
+
+public sealed class Item : AggregateRoot<Guid>
+{
+    public Guid BrandId { get; private set; }
+    public Guid CategoryId { get; private set; }
+    public decimal Price { get; private set; }
+    public string Name { get; set; }
+    public string Description { get; private set; }
+    public uint AvailableCount { get; private set; }
+    public uint ReservedCount { get; private set; }
+
+    public Item(
+        Guid brandId,
+        Guid categoryId,
+        decimal price,
+        string name,
+        string description,
+        uint count) : base(Guid.NewGuid())
+    {
+        BrandId = brandId;
+        CategoryId = categoryId;
+        Price = price;
+        Name = name;
+        Description = description;
+        AvailableCount = count;
+    }
+
+    public static Item Create(
+        Guid brandId,
+        Guid categoryId,
+        decimal price,
+        string name,
+        string description,
+        uint count)
+    {
+        return new Item(brandId,categoryId,price,name,description,count);
+    }
+
+    public void Reserve(uint count)
+    {
+        if (AvailableCount < count)
+            throw new DomainException("Invalid items count");
+
+        ReservedCount += count;
+        AvailableCount -= count;
+    }
+
+    public void Sell(uint count)
+    {
+        if (ReservedCount < count)
+            throw new DomainException("Invalid items count");
+
+        ReservedCount -= count;
+    }
+
+    public void AddItems(uint count)
+    {
+        AvailableCount += count;
+    }
+
+    public void Release(uint count)
+    {
+        if (ReservedCount < count)
+            throw new DomainException("Invalid items count");
+
+        ReservedCount -= count;
+        AvailableCount += count;
+    }
+
+#pragma warning disable CS8618
+    private Item() { }
+#pragma warning restore CS8618
+}
