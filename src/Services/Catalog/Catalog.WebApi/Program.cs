4dbd077e37b9583fb52c3eeb6a765eaa08aa753e
samantonovich@gmail.com <samantonovich@gmail.com>
2023-05-24 22:57:31 +0300

    Add exception handling middleware

--- src/Services/Catalog/Catalog.WebApi/Program.cs
+++ src/Services/Catalog/Catalog.WebApi/Program.cs
@@ -17,6 +17,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseExceptionHandler("/error");
+
 app.MapControllers();
 
 app.Run();
---
+++ src/Services/Ordering/Ordering.WebApi/Controllers/ErrorsController.cs
@@ -0,0 +1,27 @@
+﻿using BuildingBlocks.Application.Exceptions;
+using BuildingBlocks.Domain;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Ordering.WebApi.Controllers;
+
+[ApiExplorerSettings(IgnoreApi = true)]
+public class ErrorsController : ControllerBase
+{
+    [Route("/error")]
+    public IActionResult Error()
+    {
+        Exception? exception = HttpContext.Features
+            .Get<IExceptionHandlerFeature>()?.Error;
+
+        var statusCode = exception switch
+        {
+            NotFoundException => HttpStatusCode.NotFound,
+            DomainException => HttpStatusCode.BadRequest,
+            _ => HttpStatusCode.InternalServerError
+        };
+
+        return Problem(detail: exception?.Message, statusCode: (int)statusCode);
+    }
+}
