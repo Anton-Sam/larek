b5ffdb7bce0c0c0bf9d1103bafa84210711c5853
samantonovich@gmail.com <samantonovich@gmail.com>
2023-05-25 12:14:04 +0300

    Add delivery date to order

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Migrations/20230525082309_AddDeliveryDate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ordering.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeliveryDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeliveryDate",
+                table: "Orders",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeliveryDate",
+                table: "Orders");
+        }
+    }
+}
